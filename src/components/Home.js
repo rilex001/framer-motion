7f28056931ce5daa6b15957f3149022000c35ff6
rilex001 <ristic1900@gmail.com>
2020-07-24 21:43:55 +0200

    fist commit

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion'
+
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)'
+  }
+}
+
+const Home = () => {
+  return (
+    <motion.div className="home container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1.5 }}
+    >
+        <h2>Welcome to Pizza Joint</h2>
+        <Link to="/base">
+        <motion.button
+          variants={buttonVariants}
+          whileHover='hover'
+        >
+          Create Your Pizza
+        </motion.button>
+      </Link>
+    </motion.div>
+  )
+}
+
+export default Home;
